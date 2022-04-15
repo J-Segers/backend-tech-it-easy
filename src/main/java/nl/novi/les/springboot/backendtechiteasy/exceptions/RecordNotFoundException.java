d4f8002d4ff59036aff341c8a6f560503c5fc5e6
J-Segers <jethroseger@gmail.com>
2022-04-15 18:41:41 +0200

    added the RecordNotFoundException

---
+++ src/main/java/nl/novi/les/springboot/backendtechiteasy/exceptions/RecordNotFoundException.java
@@ -0,0 +1,15 @@
+package nl.novi.les.springboot.backendtechiteasy.exceptions;
+
+public class RecordNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public RecordNotFoundException() {
+        super();
+    }
+
+    public RecordNotFoundException(String message) {
+        super(message);
+    }
+
+}
