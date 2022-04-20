64f3cc0c99ba5c4b2c5885cb359e52c649738894
J-Segers <jethroseger@gmail.com>
2022-04-20 21:20:46 +0200

    TelevisionRepository now extends JpaRepository

--- src/main/java/nl/novi/les/springboot/backendtechiteasy/repositories/TelevisionRepository.java
+++ src/main/java/nl/novi/les/springboot/backendtechiteasy/repositories/TelevisionRepository.java
@@ -1,4 +1,7 @@
 package nl.novi.les.springboot.backendtechiteasy.repositories;
 
-public interface TelevisionRepository {
+import nl.novi.les.springboot.backendtechiteasy.models.Television;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelevisionRepository extends JpaRepository<Television, Long> {
 }
